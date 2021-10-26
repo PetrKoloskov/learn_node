f7618d1881b907dbec19dbaf08c9e1bfe77e1816
user <petr>
2021-10-26 16:49:26 +0300

    123

--- app2.js
+++ app2.js
@@ -2,28 +2,34 @@ const axios=require("axios");
 //const tress=require("tress");
 const url="https://www.zdorovieinfo.ru/";
 let count=100;
-//let resultLinks=[];
+let resultLinks=[];
 
+function setTree(url){
+
+  if(count==0) return;
   axios.get(url).then(response => {
     let res = response.data;
     var pt = /<a(.*?)href=\"(.*?)"(.*?)>(.*?)<\/a>/g;
     pt = new RegExp(pt);
     let found = [...res.matchAll(pt)];
-    let resultLinks=[];
+
     resultLinks[url]=[];
     for (let i = 0; i < found.length; i++) {
       if (!resultLinks[url].includes(found[i][2])) {
-        count--;
         resultLinks[url].push(found[i][2]);
-
+        count--;
+        if(count==0) {console.log(resultLinks);return;}
+        //setTree(found[i][2]);
       }
-      //if(count==0) break;
+
     }
-    console.log(resultLinks[url]);
 
   }).catch(error => {
     console.log(error);
   });
+}
+setTree(url);
+
 
 
 
